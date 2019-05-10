03625d7deef570fe424fc88674560671398a3d85
Anthiel <44339217+Anthiel@users.noreply.github.com>
2019-05-10 13:18:08 +0200

    fill presque fini

--- Space.cpp
+++ Space.cpp
@@ -6,7 +6,7 @@
 Space::Space(MyMesh* _mesh, Voxelisation voxelisationType, int size){
     this->_mesh = _mesh;
     this->voxelisationType = voxelisationType;
-    this->changeSize(size+1, size+1, size+1);
+    this->changeSize(size+1, size+1, size+1); // il y a size voxel
     this->initXYZ();
 
 }
@@ -440,20 +440,24 @@ int Space::getTotalVoxels(){
 void Space::fillWithVoxels(){
     //supprime les valeurs doubles
     deleteDuplicate();
+    qDebug() << "[DEBUG]" << "v :" << activatedVoxel;
+
     std::vector<int> newVoxel;
     for(unsigned i = 0; i < activatedVoxel.size(); i++){
-        qDebug() << "[DEBUG]" << "Valeur actuelle de i :" << i;
-        for(int k = activatedVoxel.at(i); k <= k+((largeur-1)-(k%(largeur-1))) ; k++){
+        qDebug() << "[DEBUG]" << "Valeur actuelle de k :" << activatedVoxel.at(i) << "jusqu'à : "<< activatedVoxel.at(i)+((largeur-1)-(activatedVoxel.at(i)%(largeur-1)));
+        for(int k = activatedVoxel.at(i); k <= activatedVoxel.at(i)+((largeur-1)-(activatedVoxel.at(i)%(largeur-1))) ; k++){
+            qDebug() << "\t [DEBUG]" << "sur la ligne, point actuel :" << k;
+            k++;
             if(std::find(activatedVoxel.begin(), activatedVoxel.end(), k) != activatedVoxel.end()){
-                qDebug() << "[DEBUG]" << "K trouvé :" << k;
-                for(int j = activatedVoxel.at(i); j<=k ; j++){
-                    qDebug() << "\t" << "[DEBUG]" << "Voxel activé :" << k;
+                qDebug() << "\t [DEBUG]" << "valeur trouvé dans la liste :" << k;
+                for(int j = activatedVoxel.at(i)+1; j<k ; j++){
+                    qDebug() << "\t\t" << "[DEBUG]" << "on active le Voxel n°:" << j;
                     newVoxel.push_back(j);
                 }
-                i++;
                 break;
             }
         }
+        i++;
     }
     qDebug() << "[DEBUG]" << "newVoxel :" << newVoxel;
     qDebug() << "[DEBUG]" << "v :" << activatedVoxel;
